caa00cbf2a2eab52ec5c21f417723842934050ba
Guy Bedford <guybedford@gmail.com>
2013-08-29 10:50:21 +0200

    more fixes

--- comments.js
+++ comments.js
@@ -2,7 +2,7 @@ module.exports = function removeComments(str) {
   // output
   var curOutIndex = 0,
     outString = '';
-  
+
   // mode variables
   var singleQuote = false,
     doubleQuote = false,
@@ -10,55 +10,64 @@ module.exports = function removeComments(str) {
     blockComment = false,
     doubleBackslash = false,
     lineComment = false;
-  
+
   // character buffer
   var lastChar;
   var curChar = '';
-  
+  var lastToken;
+
   for (var i = 0, l = str.length; i <= l; i++) {
     lastChar = curChar;
     curChar = str.charAt(i);
-  
+
     if (curChar === '\n' || curChar === '\r' || curChar === '') {
       regex = doubleQuote = singleQuote = doubleBackslash = false;
       if (lineComment) {
         curOutIndex = i + 1;
         lineComment = false;
       }
+      lastToken = '';
       continue;
     }
-  
+
+    if (lastChar !== ' ' && lastChar !== '\t')
+      lastToken = lastChar;
+
     if (singleQuote || doubleQuote || regex) {
       if (curChar == '\\' && lastChar == '\\')
         doubleBackslash = !doubleBackslash;
     }
-  
+
     if (singleQuote) {
       if (curChar === "'" && (lastChar !== '\\' || doubleBackslash))
         singleQuote = doubleBackslash = false;
     }
-  
+
     else if (doubleQuote) {
       if (curChar === '"' && (lastChar !== '\\' || doubleBackslash))
         doubleQuote = doubleBackslash = false;
     }
-  
+
     else if (regex) {
-      if (curChar === '/'  && (lastChar !== '\\' || doubleBackslash))
+      if (curChar === '/'  && (lastChar !== '\\' || doubleBackslash)) {
         regex = doubleBackslash = false;
+        i++;
+        lastToken = lastChar = curChar;
+        curChar = str.charAt(i);
+      }
     }
-  
+
     else if (blockComment) {
       if (curChar === '/' && lastChar === '*') {
         blockComment = false;
         curOutIndex = i + 1;
       }
     }
-  
+
     else if (!lineComment) {
       doubleQuote = curChar === '"';
       singleQuote = curChar === "'";
-  
+
       if (lastChar !== '/')
         continue;
       
@@ -73,7 +82,7 @@ module.exports = function removeComments(str) {
         lineComment = true;
         outString += str.substring(curOutIndex, i - 1);
       }
-      else {
+      else if (lastToken !== '}' && lastToken !== ')' && lastToken !== ']') {
         regex = true;
       }
     }
