936dd10cf5410b3d667035055cb7b2c3678c8d9e
Guy Bedford <guybedford@gmail.com>
2013-08-29 10:17:51 +0200

    minor rewrite for double escaping and regex v division

--- comments.js
+++ comments.js
@@ -2,66 +2,76 @@ module.exports = function removeComments(str) {
   // output
   var curOutIndex = 0,
     outString = '';
-
+  
   // mode variables
   var singleQuote = false,
     doubleQuote = false,
     regex = false,
     blockComment = false,
+    doubleBackslash = false,
     lineComment = false;
-
+  
   // character buffer
-  var lastChar, curChar, 
-    nextChar = str.charAt(0);
-
-  for (var i = 0, l = str.length; i < l; i++) {
+  var lastChar;
+  var curChar = '';
+  
+  for (var i = 0, l = str.length; i <= l; i++) {
     lastChar = curChar;
-    curChar = nextChar;
-    nextChar = str.charAt(i + 1);
-
+    curChar = str.charAt(i);
+  
+    if (curChar === '\n' || curChar === '\r' || curChar === '') {
+      regex = doubleQuote = singleQuote = doubleBackslash = false;
+      if (lineComment) {
+        curOutIndex = i + 1;
+        lineComment = false;
+      }
+      continue;
+    }
+  
+    if (singleQuote || doubleQuote || regex) {
+      if (curChar == '\\' && lastChar == '\\')
+        doubleBackslash = !doubleBackslash;
+    }
+  
     if (singleQuote) {
-      if (curChar === "'" && lastChar !== '\\')
-        singleQuote = false;
+      if (curChar === "'" && (lastChar !== '\\' || doubleBackslash))
+        singleQuote = doubleBackslash = false;
     }
-
+  
     else if (doubleQuote) {
-      if (curChar === '"' && lastChar !== '\\')
-        doubleQuote = false;
+      if (curChar === '"' && (lastChar !== '\\' || doubleBackslash))
+        doubleQuote = doubleBackslash = false;
     }
-
+  
     else if (regex) {
-      if (curChar === '/'  && lastChar !== '\\')
-        regex = false;
+      if (curChar === '/'  && (lastChar !== '\\' || doubleBackslash))
+        regex = doubleBackslash = false;
     }
-
+  
     else if (blockComment) {
       if (curChar === '/' && lastChar === '*') {
         blockComment = false;
         curOutIndex = i + 1;
       }
     }
-    
-    else if (lineComment) {
-      if (nextChar === '\n' || nextChar === '\r' || nextChar == '') {
-        lineComment = false;
-        curOutIndex = i + 1;
-      }
-    }
-
-    else {
+  
+    else if (!lineComment) {
       doubleQuote = curChar === '"';
       singleQuote = curChar === "'";
-
-      if (curChar !== '/')
+  
+      if (lastChar !== '/')
         continue;
       
-      if (nextChar === '*') {
+      if (curChar === '*') {
         blockComment = true;
-        outString += str.substring(curOutIndex, i);
+        outString += str.substring(curOutIndex, i - 1);
+        i++;
+        lastChar = curChar;
+        curChar = str.charAt(i);
       }
-      else if (nextChar === '/') {
+      else if (curChar === '/') {
         lineComment = true;
-        outString += str.substring(curOutIndex, i);
+        outString += str.substring(curOutIndex, i - 1);
       }
       else {
         regex = true;
